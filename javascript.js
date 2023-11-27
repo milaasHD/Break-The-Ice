6ba3c536d663c973814bb539bb643068e1c88d1d
babas <babas@bzz.ch>
2023-11-27 12:12:31 +0100

    Improved, Modale Dialgue

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ addEventListener("DOMContentLoaded", function (){
     function addClassOnScroll() {
         const element = document.getElementById('appear');
         const scrollPosition = window.scrollY || window.pageYOffset;
-        const targetPosition = 100; // Change this to the position where you want the class to be added
+        const targetPosition = 600; // Change this to the position where you want the class to be added
 
         if (scrollPosition >= targetPosition) {
             element.classList.add('appear');
@@ -27,5 +27,103 @@ addEventListener("DOMContentLoaded", function (){
 
         document.querySelector(this.parentElement.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'}); });
+
+
+
+
+    let modal = document.getElementById("myModal");
+    let tutmodal = document.getElementById("myModal1")
+
+
+    let classicalBtn = document.getElementById("butt");
+    let spinBtn = document.getElementById("spin");
+
+
+    let span = document.getElementsByClassName("close")[0];
+
+
+    function showModal() {
+        modal.style.display = "block";
+        document.body.style.overflow = 'hidden';
+
+    }
+    let initialChoice;
+
+    function closeModal() {
+        modal.style.display = "none";
+        document.body.style.overflow = 'auto';
+    }
+
+
+    classicalBtn.onclick = function() {
+        initialChoice = 'classical';
+        content_first.style.display = "inline"
+        content_second.style.display = "none"
+        showModal();
+    };
+
+    spinBtn.onclick = function() {
+        initialChoice = 'spin';
+        content_first.style.display = "inline"
+        content_second.style.display = "none"
+        showModal();
+    };
+    span.onclick = closeModal;
+
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            closeModal();
+        }
+    }
+
+    document.addEventListener('mousemove', function(e) {
+        let image = document.getElementById('image');
+
+        // Get the position of the image
+        const imageRect = image.getBoundingClientRect();
+
+        // Calculate the center of the image
+        const imageCenterX = imageRect.left + imageRect.width / 2;
+        const imageCenterY = imageRect.top + imageRect.height / 2;
+
+        // Calculate the angle of rotation
+        const angle = Math.atan2(e.clientY - imageCenterY, e.clientX - imageCenterX) * 180 / Math.PI;
+
+        // Apply the rotation to the image
+        image.style.transform = `rotate(${angle}deg)`;
+    });
+    let tutorialBtn = document.getElementById('tut');
+    let playFurtherBtn = document.getElementById('continue')
+
+    let continues = document.getElementById("conti")
+    let content_first = document.getElementById("TutInput")
+    let content_second = document.getElementById("TutContent")
+
+    tutorialBtn.addEventListener('click', function() {
+        content_first.style.display = "none"
+        content_second.style.display = "inline"
+
+
+
+    });
+    continues.addEventListener('click', function (){
+        if (initialChoice === 'classical') {
+            window.location.href = "Classic.html"
+        } else if (initialChoice === 'spin') {
+            window.location.href = "Wheel.html"
+        }
+    })
+    playFurtherBtn.addEventListener('click', function() {
+        closeModal();
+        if (initialChoice === 'classical') {
+            window.location.href = "Classic.html"
+        } else if (initialChoice === 'spin') {
+            window.location.href = "Wheel.html"
+        }
+    });
+
+
+
+
 })
 
